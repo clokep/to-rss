da30e7c44a3cd086e5ef46b2b0036d269161778d
Patrick Cloke <clokep@patrick.cloke.us>
2017-09-01 08:58:40 -0400

    Move templates to separate files.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Simple Flask server that combines both the NHL and Wikipedia news parsers.
 
 from flask import abort, Flask
 
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 from to_rss.nhl import nhl_news, team_news, VALID_TEAMS
 from to_rss.patreon import patreon_posts
@@ -15,32 +15,21 @@ application = Flask(__name__)
 # PythonAnywhere likes to call this 'app'.
 app = application
 
+# Jinja2 environment.
+env = Environment(loader=FileSystemLoader('to_rss/templates'))
+
 
 @application.route('/')
 def serve_about():
     """A link to each endpoint that's supported."""
-    template = Template('''<html>
-<body>
-<ul>
-<li><a href="/nhl/">NHL News</a></li>
-<li><a href="/patreon/">Patreon</a></li>
-<li><a href="/wikipedia/">Wikipedia</a></li>
-</ul>
-</body>
-</html>''')
-
+    template = env.get_template('index.html')
     return template.render()
 
 
 # Wikipedia end points.
 @application.route('/wikipedia/')
 def serve_wikipedia():
-    template = Template('''<html>
-<body>
-<h2><a href="/wikipedia/current_events/">Wikipedia Current Events</a></h2>
-</body>
-</html>''')
-
+    template = env.get_template('wikipedia.html')
     return template.render()
 
 
@@ -52,19 +41,7 @@ def serve_wikipedia_current_events():
 # NHL end points.
 @application.route('/nhl/')
 def serve_nhl():
-    template = Template('''<html>
-<body>
-<h2><a href="/nhl/news/">NHL Headlines</a></h2>
-
-<ul>
-{% for short, name in teams|dictsort(false, 'value') %}
-<li><a href="/nhl/{{ short }}/">{{ name }}</a></li>
-{% endfor %}
-</ul>
-
-</body>
-</html>''')
-
+    template = env.get_template('nhl.html')
     return template.render(teams=VALID_TEAMS)
 
 
@@ -84,13 +61,7 @@ def serve_nhl_team_news(team):
 # Patreon end points.
 @application.route('/patreon/')
 def serve_patreon():
-    template = Template('''<html>
-<body>
-<h2>Patreon Posts</h2>
-
-</body>
-</html>''')
-
+    template = env.get_template('patreon.html')
     return template.render()
 
 
